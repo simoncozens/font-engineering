49bede2d535bff8b35b634df4978fbc87561d726
Simon Cozens <simon@simon-cozens.org>
2021-01-26 09:24:32 +0000

    Make a failing test report, tweak overlap, show all

--- gnipahs.py
+++ gnipahs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from collidoscope import Collidoscope
 from vharfbuzz import Vharfbuzz
 from fontTools.ttLib import TTFont
@@ -7,9 +8,11 @@ import warnings
 import re
 from termcolor import colored
 from stringbrewer import StringBrewer
+import os
 
 
 parser = ArgumentParser(description="Shaping regression tests")
+parser.add_argument("--show-all", help="Adding passing tests to output file", action="store_true")
 parser.add_argument("font", help="Font file", metavar="FONT")
 parser.add_argument("testfile", help="File containing tests", metavar="TESTFILE")
 args = parser.parse_args()
@@ -18,17 +21,19 @@ vhb = Vharfbuzz(args.font)
 col = Collidoscope(
     args.font,
     {
-        "cursive": True,
+        #"cursive": True,
         "marks": True,
         "faraway": True,
         "adjacentmarks": False,
-        "area": 0.01,
+        # "area": 0,
     },
 )
 
 tests = []
 ingredients = []
 seen_blank = False
+failed = []
+general_options = {}
 
 with open(args.testfile, "r") as testfile:
     for line in testfile.read().split("\n"):
@@ -38,6 +43,9 @@ with open(args.testfile, "r") as testfile:
             seen_blank = True
         if seen_blank:
             ingredients.append(line)
+        elif line.startswith("["):
+            m = re.match(r"^\[(\w+)=([^]]+)\]$",line)
+            general_options[m[1]] = m[2]
         else:
             elements = line.split(":")
             if elements[0].startswith('"') and elements[0].endswith('"'):
@@ -55,10 +63,13 @@ with open(args.testfile, "r") as testfile:
 
 tested = 0
 passed = 0
+failreport = []
 
 
 def tfail(test, string, message):
     global tested
+    global failed
+    failed.append(string)
     tested += 1
     print(f"{colored('🗴 ', 'red')} '{string}' {message}")
 
@@ -66,9 +77,12 @@ def tfail(test, string, message):
 def tpass(test, string, message):
     global tested
     global passed
+    global failed
+    if args.show_all:
+        failed.append(string)
     tested += 1
     passed += 1
-    print(f"{colored('🗸 ', 'green')} '{string}' {message}")
+    # print(f"{colored('🗸 ', 'green')} '{string}' {message}")
 
 
 def do_test(string, options):
@@ -78,7 +92,13 @@ def do_test(string, options):
         tfail(test, string, "overlap test")
     else:
         tpass(test, string, "overlap test")
-
+    if "forbidden" in general_options:
+        forbidden = general_options["forbidden"].split(",")
+        glyphnames = [x["name"] for x in glyphs]
+        if any(x in forbidden for x in glyphnames):
+            tfail(test, string, "forbidden glyph")
+        else:
+            tpass(test, string, "forbidden glyphs")
     if len(test["options"]) > 0:
         buf = vhb.shape(string)
         expected = options[0]
@@ -90,6 +110,8 @@ def do_test(string, options):
 
 
 for test in tests:
+    failed = []
+    failreport.append( (test["string"], failed ))
     if test["type"] == "pattern":
         b = StringBrewer(from_string=test["string"] + "\n" + "\n".join(ingredients))
         try:
@@ -102,3 +124,21 @@ for test in tests:
         do_test(test["string"], test["options"])
 
 print("\n%i tests, %i passed, %i failing" % (tested, passed, tested - passed))
+
+slug = " \\qquad "
+# All your failing tests as a PDF
+with open("test-failures.sil", "w") as sil:
+    sil.write(f"""\\begin{{document}}
+\\font[filename="{args.font}"]
+
+\\script[src=packages/linespacing]
+\\set[parameter=linespacing.method,value=fixed]
+\\set[parameter=linespacing.fixed.baselinedistance,value=3em]
+""")
+
+    for (string, failed) in failreport:
+        sil.write("\\bigskip \\font[family=Gentium Plus]{%s}\\bigskip" % string)
+        sil.write(slug.join(failed))
+    sil.write(r"\end{document}")
+os.system("sile test-failures.sil >/dev/null 2>/dev/null")
+os.system("open test-failures.pdf")
