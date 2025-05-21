55080ec497ae2dc3ab1c94bb5951bacb264ef5d6
Simon Cozens <simon@simon-cozens.org>
2025-05-21 13:14:17 +0100

    Improve name ID display

--- dump-names.py
+++ dump-names.py
@@ -6,41 +6,34 @@ import sys
 
 
 font = TTFont(sys.argv[1])
-names = {}
-for record in font["name"].names:
-    if record.nameID < 255:
-        names[record.nameID] = record.toUnicode()
-
-table = Table(title="name")
-table.add_column("Name ID", justify="right", style="cyan")
-table.add_column("Name", justify="left")
-for nameID in sorted(names.keys()):
-    table.add_row(str(nameID), names[nameID])
-console = Console()
-console.print(table)
+seen_ids = set()
 
 if "fvar" in font:
-    table = Table(title="fvar instances")
-    table.add_column("Name ID", justify="right", style="cyan")
-    table.add_column("Name", no_wrap=True)
-    table.add_column("Coordinates", no_wrap=True)
+    fvar_table = Table(title="fvar instances")
+    fvar_table.add_column("Name ID", justify="right", style="cyan")
+    fvar_table.add_column("Name", no_wrap=True)
+    fvar_table.add_column("Coordinates", no_wrap=True)
     for instance in font["fvar"].instances:
         name = font["name"].getDebugName(instance.subfamilyNameID)
+        if instance.subfamilyNameID > 255:
+            seen_ids.add(instance.subfamilyNameID)
         coords = ", ".join(
             f"{font['fvar'].axes[i].axisTag}: {instance.coordinates[font['fvar'].axes[i].axisTag]}"
             for i in range(len(font["fvar"].axes))
         )
-        table.add_row(str(instance.subfamilyNameID), name, coords)
-    console.print(table)
+        fvar_table.add_row(str(instance.subfamilyNameID), name, coords)
 
 if "STAT" in font:
-    table = Table(title="STAT")
-    table.add_column("Name ID", justify="right", style="cyan")
-    table.add_column("Name", no_wrap=True)
-    table.add_column("Coordinate", no_wrap=True)
+    stat_table = Table(title="STAT")
+    stat_table.add_column("Name ID", justify="right", style="cyan")
+    stat_table.add_column("Name", no_wrap=True)
+    stat_table.add_column("Coordinate", no_wrap=True)
     axes = [a.AxisTag for a in font["STAT"].table.DesignAxisRecord.Axis]
     for particle in font["STAT"].table.AxisValueArray.AxisValue:
         name = font["name"].getDebugName(particle.ValueNameID)
+        if particle.ValueNameID > 255:
+            seen_ids.add(particle.ValueNameID)
+
         if particle.Format == 1:
             coords = f"{axes[particle.AxisIndex]}: {particle.Value}"
         elif particle.Format == 2:
@@ -52,5 +45,21 @@ if "STAT" in font:
             coords = ", ".join(
                 f"{axes[av.AxisIndex]}={av.Value}" for av in particle.AxisValueRecord
             )
-        table.add_row(str(particle.ValueNameID), name, coords)
-    console.print(table)
+        stat_table.add_row(str(particle.ValueNameID), name, coords)
+
+names = {}
+for record in font["name"].names:
+    if record.nameID not in seen_ids:
+        names[record.nameID] = record.toUnicode()
+
+name_table = Table(title="name")
+name_table.add_column("Name ID", justify="right", style="cyan")
+name_table.add_column("Name", justify="left")
+for nameID in sorted(names.keys()):
+    name_table.add_row(str(nameID), names[nameID])
+console = Console()
+console.print(name_table)
+if "fvar" in font:
+    console.print(fvar_table)
+if "STAT" in font:
+    console.print(stat_table)
